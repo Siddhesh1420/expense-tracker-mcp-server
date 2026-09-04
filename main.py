0803f4f85d31b4cd8fd8ecb96526e102b4e83de6
Siddhesh1420 <siddheshdiptil@gmail.com>
2026-09-04 16:03:36 +0530

    Debugging

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fastmcp import FastMCP
-# from fastmcp.server.auth.providers.google import GoogleProvider
 import os
 import sqlite3
 import json
-from fastmcp.server.dependencies import get_access_token
+from fastmcp.server.dependencies import get_http_request
 import aiosqlite
 
 DB_Path=os.getenv("DB_PATH",os.path.join(os.path.dirname(__file__), "expenses.db"))
@@ -30,8 +29,14 @@ get_db_connection()
 @mcp.tool()
 async def add_expense(date,amount,category,subcategory='',note=''):
     """Add an expense to the database."""
-    token = get_access_token()
-    print("AUTH CLAIMS:", token.claims)
+    request = get_http_request()
+
+    print("REQUEST USER:", request.user)
+    print("USER TYPE:", type(request.user))
+    print(
+        "USER ATTRS:",
+        vars(request.user) if hasattr(request.user, "__dict__") else "NO __dict__"
+    )
     async with aiosqlite.connect(DB_Path) as conn:
         cur=await conn.execute("INSERT INTO expenses (amount, category, date, subcategory, note) VALUES (?, ?, ?, ?, ?)",(amount, category, date, subcategory, note))
         await conn.commit()
