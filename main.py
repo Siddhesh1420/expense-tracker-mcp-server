ab5eea25887d157df20f515bc924d2817511805f
Siddhesh1420 <siddheshdiptil@gmail.com>
2026-09-04 14:09:41 +0530

    Resolved database issue

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from fastmcp import FastMCP
-from fastmcp.server.auth.providers.google import GoogleProvider
+# from fastmcp.server.auth.providers.google import GoogleProvider
 import os
 import sqlite3
 import json
-import tempfile
+
 import aiosqlite
 
-TEMP_DIR = tempfile.gettempdir()
-DB_Path=os.path.join(TEMP_DIR, "expenses.db")
+DB_Path=os.getenv("DB_PATH",os.path.join(os.path.dirname(__file__), "expenses.db"))
 CATEGORIES_path=os.path.join(os.path.dirname(__file__), "category.json")
 
-auth = GoogleProvider(client_id=os.getenv("GOOGLE_CLIENT_ID") or "",client_secret=os.getenv("GOOGLE_CLIENT_SECRET") or "",base_url="https://friendly-magenta-ant.fastmcp.app")
-# Create a FastMCP server instance
-mcp = FastMCP(name="Expense Tracker",auth=auth)
+# auth = GoogleProvider(client_id=os.getenv("GOOGLE_CLIENT_ID") or "",client_secret=os.getenv("GOOGLE_CLIENT_SECRET") or "",base_url="https://friendly-magenta-ant.fastmcp.app")
+# # Create a FastMCP server instance
+# mcp = FastMCP(name="Expense Tracker",auth=auth)
+mcp = FastMCP(name="Expense Tracker")
 
 def get_db_connection():
     """Get a connection to the SQLite database."""
